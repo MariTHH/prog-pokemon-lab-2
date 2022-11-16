c1c30154057f874511a248a06b019157ee0252af
MariTHH <118300117+MariTHH@users.noreply.github.com>
2022-11-16 17:16:04 +0300

    Add files via upload

---
+++ src/moves/SeedBomb.java
@@ -0,0 +1,13 @@
+package moves;
+
+import ru.ifmo.se.pokemon.*;
+
+public class SeedBomb extends SpecialMove {
+    public SeedBomb(){
+        super(Type.GRASS,80,100);
+    }
+    @Override
+    protected String describe(){
+        return "использует Seed Bomb";
+    }
+}
